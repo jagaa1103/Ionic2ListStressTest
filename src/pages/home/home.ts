454b80079ca0d171ded534b138b3049c59d404d4
jagaa <jagaa1103@gmail.com>
2017-05-26 18:24:02 +0900

    testing done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  normal_items = [];
+  virtual_items = [];
 
+  constructor(public navCtrl: NavController) {
+    
   }
 
+  ionViewDidEnter(){
+    console.log("ionViewDidEnter");
+    
+  }
+
+  createNormalList(){
+    this.virtual_items = [];
+    this.initNormal();
+  }
+  createVirtualList(){
+    this.normal_items = [];
+    this.initVirtual();
+  }
+
+  initVirtual(){
+    this.virtual_items = [];
+    for(let i = 0; i < 1000; i++) {
+      this.virtual_items.push({
+        title: "virtual item#" + i,
+        desc: "description of virtual item" + i
+      });
+    }
+  }
+
+  initNormal(){
+    this.normal_items = [];
+    for(let i = 0; i < 1000; i++) {
+      this.normal_items.push({
+        title: "normal item#" + i,
+        desc: "description of normal item" + i
+      });
+    }
+  }
+
+  clearList(){
+    this.normal_items = [];
+    this.virtual_items = [];
+  }
 }
